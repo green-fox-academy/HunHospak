b73a2c78a1c85ddc45032c8492acedc863746d54
HunHospak <hospak88@gmail.com>
2019-10-29 09:32:42 +0100

    Added submit button

--- week7/day01/bank/src/main/java/simba/bank/controllers/BankAccountController.java
+++ week7/day01/bank/src/main/java/simba/bank/controllers/BankAccountController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import simba.bank.models.BankAccount;
 
 @Controller
@@ -27,14 +27,13 @@ List<BankAccount> bankAccounts;
     return "index";
   }
 
-  @PostMapping(value = "/add/{id}")
-  public String addPlusMoney(@PathVariable(name = "id") Integer id, Model model) {
-    if (bankAccounts.get(id).isKing()) {
-      bankAccounts.get(id).setBalance(bankAccounts.get(id).getBalance()+100);
-    } else {
-      bankAccounts.get(id).setBalance(bankAccounts.get(id).getBalance()+10);
+  @PostMapping(value ="/raise")
+  public String raise(Model model, @RequestParam int animalIndex){
+    if(bankAccounts.get(animalIndex).isKing()){
+      bankAccounts.get(animalIndex).setBalance(bankAccounts.get(animalIndex).getBalance() + 100);
+    }else{
+      bankAccounts.get(animalIndex).setBalance(bankAccounts.get(animalIndex).getBalance() + 10);
     }
-    model.addAttribute("animalList", bankAccounts);
     return "redirect:/";
   }
 }
